1accb7e8cb17706c6e6ef6e527330a8d1d6b69e8
Clement-R <rondeau.clement@gmail.com>
2017-05-16 16:31:03 +0200

    Init project

---
+++ Assets/ColorUtilityTest/Scripts/ChangeColor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ColorUtilityTest {
+    public class ChangeColor : MonoBehaviour {
+        public Renderer re;
+        public string htmlColor = "#2ecc71";
+
+        Color col;
+
+        void Start() {
+            UpdateColor("#2ecc71");
+        }
+
+        void UpdateColor(string htmlColor) {
+            ColorUtility.TryParseHtmlString(htmlColor, out col);
+            re.sharedMaterial.color = col;
+        }
+
+        void OnValidate() {
+            UpdateColor(htmlColor);
+        }
+    }
+}
