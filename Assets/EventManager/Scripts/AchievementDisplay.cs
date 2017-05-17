87b705ae4e6f47636362ccd00829014383ad1c2e
Clement-R <rondeau.clement@gmail.com>
2017-05-17 10:46:16 +0200

    Add EventManager module, with achievement system as a working example

---
+++ Assets/EventManager/Scripts/AchievementDisplay.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using pkm.EventManager;
+using UnityEngine.UI;
+
+public class AchievementDisplay : MonoBehaviour {
+    public GameObject achievementPanel;
+    Text panelText;
+
+	void Start () {
+        panelText = achievementPanel.transform.GetChild(0).GetComponent<Text>();
+    }
+	
+    void OnEnable() {
+        EventManager.StartListening("playerMove", OnPlayerMove);
+    }
+
+    void OnDisable() {
+        EventManager.StopListening("playerMove", OnPlayerMove);
+    }
+
+    void OnPlayerMove() {
+        Debug.Log("A player moved !");
+        UnlockAchievement("You made it !");
+    }
+
+    void UnlockAchievement(string text) {
+        achievementPanel.SetActive(true);
+        panelText.text = text;
+        StartCoroutine("hidePanel");
+    }
+
+    IEnumerator hidePanel() {
+        yield return new WaitForSeconds(2.0f);
+        achievementPanel.SetActive(false);
+    }
+}
---
+++ Assets/EventManager/Scripts/TriggerAchievement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using pkm.EventManager;
+
+public class TriggerAchievement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)) {
+            EventManager.TriggerEvent("playerMove");
+        }
+	}
+}
