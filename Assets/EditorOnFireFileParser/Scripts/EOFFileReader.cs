bd4d0f33b59de5c35202bb08f85750e1baaa8472
Clement-R <rondeau.clement@gmail.com>
2017-07-02 13:59:50 +0200

    Started to work on eof file parser. Just poking around for the moment.

---
+++ Assets/EditorOnFireFileParser/Scripts/EOFFileReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace EditorOnFireFileParser {
+    public class EOFFileReader : MonoBehaviour {
+
+        void Start() {
+            // Read file
+            byte[] buffer = File.ReadAllBytes("Assets/EditorOnFireFileParser/Resources/notes_2.eof");
+            
+            char[] rheader = new char[16];
+
+            for (int i = 0; i < 16; i++) {
+                rheader[i] = Convert.ToChar(buffer[i]);
+
+                Debug.Log(rheader[i]);
+            }
+
+            for (int i = 0; i < 4; i++) {
+                rheader[i] = Convert.ToChar(buffer[i]);
+
+                Debug.Log(rheader[i]);
+            }
+        }
+    }
+}
