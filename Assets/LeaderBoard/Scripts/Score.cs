f339929c4b8eaf2528a7d2ec3ac9a4b85da7d5f8
C.Rondeau <rondeau.clement@gmail.com>
2018-01-14 16:04:42 +0100

    Add a leaderboard example using a custom API

---
+++ Assets/LeaderBoard/Scripts/Score.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Score {
+
+    public string name;
+    public string score;
+    public string date;
+}
+
+[System.Serializable]
+public class ScoreList
+{
+    public List<Score> Score;
+}
