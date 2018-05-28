78d4b42d0e8581ae270f96f1110a150269a75d5a
Clement-R <rondeau.clement@gmail.com>
2018-05-28 22:50:08 +0200

    Add procedural lightning start

---
+++ Assets/CardBoosterOpening/BoosterHeadScrapPiece.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoosterHeadScrapPiece : MonoBehaviour {
+
+    public bool isActive = true;
+
+    private Rigidbody2D _rb2d;
+
+    private void Start()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+        _rb2d.bodyType = RigidbodyType2D.Static;
+    }
+
+    private void OnMouseDown()
+    {
+        _rb2d.bodyType = RigidbodyType2D.Dynamic;
+        float rand = Random.Range(0f, 1f) > 0.5f ? 1f : -1f;
+        _rb2d.AddForce(new Vector2(rand * 15f, 10f), ForceMode2D.Impulse);
+
+        isActive = false;
+    }
+}
---
+++ Assets/DragAndDropCards/GrabbableUIElement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class GrabbableUIElement : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.DOMove(new Vector2(mousePosition.x, mousePosition.y), 0.15f).SetEase(Ease.InOutExpo);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Drag : " + gameObject.name);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.DOMove(new Vector2(mousePosition.x, mousePosition.y), 0.05f).SetEase(Ease.InOutExpo);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("Stop Drag : " + gameObject.name);
+    }
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    
+}
