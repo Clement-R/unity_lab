5e73fa10202c1b1455e32567153d7b8bc30bf591
Clement-R <rondeau.clement@gmail.com>
2018-06-05 23:49:02 +0200

    Add chain reaction

--- Assets/LightningBehaviour.cs
+++ Assets/LightningBehaviour.cs
@@ -17,6 +17,59 @@ public class LightningBehaviour : MonoBehaviour {
         StartCoroutine(Fire(end));
     }
 
+    private void ResetLine(Vector2 start)
+    {
+        _line.SetPosition(0, start);
+        _line.SetPosition(1, start);
+    }
+
+    private void DestroyLine()
+    {
+        Destroy(gameObject);
+    }
+
+    private IEnumerator CastNextLightning(GameObject enemy)
+    {
+        Debug.Log("FIRE");
+        Debug.Log("Target : " + enemy.name);
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(enemy.transform.position, 500f);
+
+        GameObject nearest = null;
+        float distance = 9999f;
+        foreach (var collider in colliders)
+        {
+            if(collider.CompareTag("Enemy") && collider.gameObject != enemy.gameObject)
+            {
+                float dist = Vector2.Distance(collider.gameObject.transform.position, enemy.transform.position);
+                if (dist <= distance)
+                {
+                    nearest = collider.gameObject;
+                    distance = dist;
+                }
+            }
+        }
+
+        // TODO : Change for kill behaviour
+        ResetLine(enemy.transform.position);
+        Destroy(enemy.gameObject);
+
+        if(nearest != null)
+        {
+            Debug.Log("FIRE NEXT ONE");
+            Debug.Log("Nearest : " + nearest.name);
+            
+            // Cast line to next enemy
+            _line.SetPosition(1, nearest.transform.position);
+
+            yield return new WaitForSeconds(0.15f);
+
+            StartCoroutine(CastNextLightning(nearest.transform.gameObject));
+        }
+
+        yield return null;
+    }
+
     private IEnumerator Fire(Vector2 end)
     {
         float t = 0f;
@@ -25,26 +78,32 @@ public class LightningBehaviour : MonoBehaviour {
             t += Time.deltaTime;
 
             Vector2 start = new Vector2(_line.GetPosition(0).x, _line.GetPosition(0).y);
-            Vector2 lineEnd = new Vector2(_line.GetPosition(1).x, _line.GetPosition(1).y); 
+            Vector2 lineEnd = new Vector2(_line.GetPosition(1).x, _line.GetPosition(1).y);
             Vector2 direction = (end - start).normalized;
             float distance = Vector2.Distance(start, lineEnd);
 
             RaycastHit2D[] hits = Physics2D.RaycastAll(start, direction, distance);
 
+            bool hasHit = false;
             foreach (var hit in hits)
             {
                 if(hit.transform.CompareTag("Enemy"))
                 {
-                    Debug.Log("HIT AN ENEMY");
-                    Debug.DebugBreak();
+                    hasHit = true;
+                    StartCoroutine(CastNextLightning(hit.transform.gameObject));
+                    break;
                 }
+            }
+
+            if(hasHit)
+            {
+                yield break;
             }
 
             yield return null;
         }
-
+        
         yield return new WaitForSeconds(0.25f);
-
-        _line.SetPosition(1, Vector2.zero);
+        DestroyLine();
     }
 }
